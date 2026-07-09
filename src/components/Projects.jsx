eafc824f6772806b3e30236427bbc30a11931f52
Luis Felipe de Paula Costa <lfbmrj15@gmail.com>
2026-07-09 20:32:52 +0000

    feat: add 4 municipality projects to portfolio
    - GIP - Gestão Interna de Processos
    - Portal do Servidor
    - ADIT - Administração de Ativos e TI
    - Fábrica de Óculos
    
    All with live URLs and screenshots. New projects displayed first.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,6 +6,11 @@ import SiteFlexTurismo from "../assets/projects/site-flex-turismo.png";
 import SiteCoffeeDelivery from "../assets/projects/site-coffee-delivery.png";
 import SiteToDoList from "../assets/projects/site-to-do-list.png";
 import SiteValidatedLogin from "../assets/projects/site-react-validated-login.png";
+// Novos projetos da Prefeitura
+import SiteGip from "../assets/projects/site-gip.png";
+import SitePortalServidor from "../assets/projects/site-portal-servidor.png";
+import SiteAdit from "../assets/projects/site-adit.png";
+import SiteFabricaOculos from "../assets/projects/site-fabrica-oculos.png";
 import ProjectCard from "./ProjectCard.jsx";
 
 export default function Projects() {
@@ -26,7 +31,13 @@ export default function Projects() {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {/* Novos projetos da Prefeitura - primeiro */}
+          <ProjectCard title={"GIP - Gestão Interna de Processos"} img={SiteGip} link={'https://gip.barramansa.rj.gov.br/'} codigo={'https://github.com/LuPeBreak/gip'} />
+          <ProjectCard title={"Portal do Servidor"} img={SitePortalServidor} link={'https://portaldoservidor.barramansa.rj.gov.br/'} codigo={'https://github.com/LuPeBreak/portal-servidor'} />
+          <ProjectCard title={"ADIT - Administração de Ativos e TI"} img={SiteAdit} link={'https://ti.barramansa.rj.gov.br/'} codigo={'https://github.com/LuPeBreak/adit'} />
+          <ProjectCard title={"Fábrica de Óculos"} img={SiteFabricaOculos} link={'https://fabricadeoculos.barramansa.rj.gov.br/'}  />
           
+          {/* Projetos anteriores */}
           <ProjectCard title={"Coffee Delivery"} img={SiteCoffeeDelivery} link={'https://lupebreak.github.io/coffee-delivery/'} codigo={'https://github.com/LuPeBreak/coffee-delivery'} />
           <ProjectCard title={"Site de Barra Mansa"} img={SiteBarraMansa} link={'https://www.barramansa.rj.gov.br/'}  />
           <ProjectCard title={"Portal da Transparencia"} img={SitePortalTransparencia} link={'https://portaltransparencia.barramansa.rj.gov.br/'} />
@@ -34,7 +45,6 @@ export default function Projects() {
           <ProjectCard title={"To Do List"} img={SiteToDoList} link={'https://lupebreak.github.io/To-Do-List-Ignite-challenge/'} codigo={'https://github.com/LuPeBreak/To-Do-List-Ignite-challenge'} />
           <ProjectCard title={"Site Flex Turismo"} img={SiteFlexTurismo} link={'https://lupebreak.github.io/flex-turismo/'} codigo={'https://github.com/LuPeBreak/flex-turismo'} />
           <ProjectCard title={"React Validated Login"} img={SiteValidatedLogin} link={'https://lupebreak.github.io/react-validated-login-desafio-4/'} codigo={'https://github.com/LuPeBreak/react-validated-login-desafio-4'} />
-          
         </div>
       </div>
     </div>
